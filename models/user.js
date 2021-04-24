d14b7c021a932ab05d523eb73a461492d565b926
jouzu1 <jouzu1@gmail.com>
2021-04-24 13:40:06 +0700

    dokumentasi entity/model user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,17 @@
+/**
+ * Nah setelah membuat servernya, kita bikin entitynya/modelnya
+ */
+
+//Setiap file membutuhkan sambungan ke mongoose/MONGO DB nya ya guys
 const mongoose = require('mongoose');
 
+
+//Nah disini pembuatan key dan value untuk data ke databasenya dalam bentuk JSON, ya paham kan istilah Schema??
+//jadi dari database akses ke schema, kaya konsep OOP aja, akses objek
 const schema = mongoose.Schema;
 
+
+//Nah ini struktu data JSON nya, nama key nya beserta tipe data valuenya
 const userSchema = new schema({
     fullname:{
         type:String
@@ -17,4 +27,5 @@ const userSchema = new schema({
     }
 })
 
+//Setelah itu entity ini kita lempar sebagai variabel bernama 'user' menggunakan model
 module.exports = mongoose.model('user',userSchema);
