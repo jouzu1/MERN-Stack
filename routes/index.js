50c1c3c87b735e68d2b330c067b65bf4ec65b9ff
jouzu1 <jouzu1@gmail.com>
2021-04-24 13:57:25 +0700

    dokumentasi routes index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
+/**
+ * Nah ini configurasi endpointnya di awal2, nanti dilempar oleh module.export ke user.js di routes
+ */
 const express = require('express');
+
+//Butuh routernya si express
 const router = express.Router();
+
+//Ini bukan entity/model dari user ya, tapi user yang ada di routes
 const user = require('./user');
 
+
+//Si router menggunakan user
 router.use('/user',user);
+
+
+//routernya dilempar atau di ekspor
 module.exports = router; 
