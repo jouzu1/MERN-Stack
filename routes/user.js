5be8d883b5a827dbc56e5133b1fb93ed975cc669
jouzu1 <jouzu1@gmail.com>
2021-04-24 14:05:24 +0700

    dokumentasi routes user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,20 @@
+/**
+ * Disini kita bisa pake endpointnya, tapi dibikin dulu endpointnya
+ * Endpointnya aja
+ */
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/userController');
 
+
+/**
+ * Oke jadi disini endpointnya, kita tetep membutuhkan nama method
+ * Methodnya untuk GET,POST, dan DELETE
+ * Ibaratnya, kita menggunakan interface yang sudah memakai repo dan interface dipakai di model servicenya
+ * Setelah itu kita pakai modelnya di controller 
+ * Nah sama, controller di userController seperti itu alurnya, menggunakan cara poin ke 3-4
+ * Disini mengunakan method2 untuk REST API dari userController, dimana isi userController menggunakan entity/model user
+ */
 router.get('/',UserController.index);
 router.post('/',UserController.create);
 router.get('/:id',UserController.details);
